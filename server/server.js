6d748244a73676c722180eea023b4a8ca1c11840
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-02-14 21:33:32 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `keep all context of response around health and fitness. do not answer question on other topics. ${prompt}`,
+      prompt: `keep all context of response around health, fitness, eating healthy, healthy meals, training and mindfulness. do not answer question on other topics. ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
