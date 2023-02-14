e0d82c3795ab54989f37badc1a0b3417a848cf8d
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-02-14 15:42:14 +0000

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = document.querySelector('formchat')
+const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')
 
 let loadInterval
