f3bb05b128a34bbfb4287832ab6748791a959021
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-02-14 21:34:32 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.post('/', async (req, res) => {
 
   } catch (error) {
     console.error(error)
-    res.status(500).send(error || 'Something went wrong');
+    res.status(500).send(error || 'Something went wrong, try again shortly');
   }
 })
 
