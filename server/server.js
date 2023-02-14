7e9c3d931899a35d9da744b45e2241a37490a4e1
daz2023 <125355767+daz2023@users.noreply.github.com>
2023-02-14 21:26:45 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,9 +27,9 @@ app.post('/', async (req, res) => {
 
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: `keep all context of response around health and fitness. do not answer question on other topics. ${prompt}`,
       temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 100, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      max_tokens: 200, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
       presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
